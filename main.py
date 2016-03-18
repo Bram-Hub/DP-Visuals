6892f5e72daaa93165536f38a6725afe52395200
Shane Aston <astonshane@gmail.com>
2016-03-18 13:33:47 -0400

    error handling for missing file; comments in main

--- main.py
+++ main.py
@@ -4,30 +4,47 @@ import statement
 import satisfiable
 
 
+# read an argument from a file and return a set of the statements contained within
 def open_argument(filename):
-    f = open(filename)
+    try:
+        f = open(filename)  # open the file
+    except:
+        print "Could not open file: %s" % filename
+        exit(1)
+
     stmt_set = []
     for line in f:
-        line = line.strip().replace(" ", "")
-        stmt = parse.parse(line)
+        line = line.strip().replace(" ", "")  # strip all of the whitespace
+        stmt = parse.parse(line)  # parse the line to get a statement
+
+        # make sure that the line was properly parsed
         if stmt is None:
             print "Parsing Error: %s" % line
             exit(1)
-        stmt_set.append(stmt)
 
+        stmt_set.append(stmt)  # add the statement to the set
+
+    # take the last statement out of the set, and negate it
     conclusion = stmt_set[-1]
     neg = statement.Statement("~", conclusion)
-
     stmt_set = stmt_set[:-1]
+
+    # re-add the negated conclusion to the set
     stmt_set.append(neg)
+
+    # return the set
     return stmt_set
 
 if __name__ == "__main__":
+    # ensure proper usage
     if len(sys.argv) != 2:
         print "Incorrect Usage: python main.py <input file>"
         exit(1)
 
+    # get a statement set from the given file
     stmt_set = open_argument(sys.argv[1])
+
+    # call the Satisfiable() algorithm to determine whether or not the argument is valid
     if satisfiable.satisfiable(stmt_set):
         print "Satisfiable! Therefore, Invalid Argument!"
     else:
