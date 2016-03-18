43fda23a80c3254f0e8e5617815d48d606d99873
Shane Aston <astonshane@gmail.com>
2016-03-18 14:39:23 -0400

    comments in satisfiable / statement

--- davisputnam/satisfiable.py
+++ davisputnam/satisfiable.py
@@ -1,36 +1,58 @@
 import parse
 
 
+# determine the next atom to be split on
 def findNextSplit(stmt_set):
-    # pick most common literal
+    # pick most common atom
+
     matches = {}
+    # for each statement in the set
     for stmt in stmt_set:
+        # get the list of atoms from each statement
         contains = stmt.contains()
+        # for each atom in the list
         for c in contains:
+            # update the count for how many times an atom has been seen
             c_count = matches.get(c, 0)
             matches[c] = c_count + 1
 
+    # return the atom which has occured the most times
     return max(matches.iterkeys(), key=(lambda key: matches[key]))
 
 
+# the Satisfiable() algorithm
 def satisfiable(stmt_set):
+
     if stmt_set == []:
+        # if the set is empty, the starting set was Satisfiable, OPEN BRANCH
         return True
     if False in stmt_set:
+        # if there is a False in the set, then something was unsatisfiable, CLOSE BRANCH
         return False
 
-    nxt_lit = findNextSplit(stmt_set)
-    nxt_l = parse.parse(nxt_lit)
-    nxt_r = parse.parse("~(%s)" % nxt_lit)
-    l = []
-    r = []
+    # determine the next atom to split on
+    nxt_atm = findNextSplit(stmt_set)
+
+    # get Statements of this atom and its negation
+    nxt_l = parse.parse(nxt_atm)
+    nxt_r = parse.parse("~(%s)" % nxt_atm)
+
+    # create two new statement sets for the left and right branches
+    l = []  # for the statements reduced on the "positive" atom
+    r = []  # for the statements reduced on the "negative" atom
+
     for stmt in stmt_set:
+        # reduce the statement on the "positive" atom
         stmt_l = stmt.reduce(nxt_l)
         if stmt_l is not True:
+            # don't add it to the new list if the recution was a True
             l.append(stmt_l)
 
+        # reduce the statement on the "negative" atom
         stmt_r = stmt.reduce(nxt_r)
         if stmt_r is not True:
+            # don't add it to the new list if the recution was a True
             r.append(stmt_r)
 
+    # return ether the left or right branch
     return satisfiable(l) or satisfiable(r)
--- davisputnam/statement.py
+++ davisputnam/statement.py
@@ -1,11 +1,14 @@
 import parse
 
 
+# class for holding a logic Statement
 class Statement:
+    # initilize the Statement
     def __init__(self, kind, value1, value2=None):
-        self.type = kind
-        self.values = [value1, value2]
+        self.type = kind  # statement type (lit, ~, v, ^, ->, <->)
+        self.values = [value1, value2]  # value2 is None if the type/connector is not binary
 
+    # for printing / outputing the statement
     def __str__(self):
         if self.type == "lit":
             return "%s" % self.value1()
@@ -14,21 +17,23 @@ class Statement:
         else:
             return "%s(%s, %s)" % (self.type, self.value1(), self.value2())
 
+    # for print / outputing the statement
     def __repr__(self):
         return self.__str__()
 
+    # quick & dirty equality operator
     def __eq__(self, other):
         return self.__str__() == other.__str__()
 
-    def __deepcopy__(self):
-        return parse.parse(self.__str__())
-
+    # returns the "left" half of the statement (or the only part if connector is non-binary)
     def value1(self):
         return self.values[0]
 
+    # returns the "right" half of the statement
     def value2(self):
         return self.values[1]
 
+    # returns a list of the atoms in a statement (Statement(~(Av~B)).contains() == [A, B])
     def contains(self):
         if self.type == "lit":
             return [self.value1()]
@@ -39,65 +44,90 @@ class Statement:
             c.extend(self.value2().contains())
             return c
 
+    # recursively reduces a statement based on a second statement
     def reduce(self, stmt):
         opp = None
+        # determine the opposite of stmt
         if stmt.type == "lit":
             opp = parse.parse("~(%s)" % stmt.__str__())
         else:
             opp = stmt.value1()
 
+        # if this statement is the same as the one we are reducing on then this statment is True
         if self == stmt:
             return True
+        # if this statement is the negation of the one we are reducing on, this statement is False
         elif self == opp:
             return False
         else:
-            if self.type == "lit":
+            if self.type == "lit":  # the statement is a literal
+                # can't recurse any farther, so just return self
                 return self
-            elif self.type == "~":
+
+            elif self.type == "~":  # the statement is a negation
+                # reduce whatever we are negating first
                 new_v1 = self.value1().reduce(stmt)
-                if type(new_v1) == bool:
+
+                if type(new_v1) == bool:  # the new statement was a boolean
+                    # return its opposite
                     return not new_v1
                 else:
+                    # return the negation of whatever we got back
                     return Statement("~", new_v1)
             else:
+                # reduce the two components of the statement first
                 new_v1 = self.value1().reduce(stmt)
                 new_v2 = self.value2().reduce(stmt)
 
-                if self.type == "v":
+                if self.type == "v":  # the statement was a disjunction
                     if new_v1 is True or new_v2 is True:
+                        # either was True, so return whole is True
                         return True
                     elif new_v1 is False:
+                        # first was False, return second
                         return new_v2
                     elif new_v2 is False:
+                        # second was False, return first
                         return new_v1
 
-                elif self.type == "^":
+                elif self.type == "^":  # the statement was a conjuction
                     if new_v1 is False or new_v2 is False:
+                        # either is False, return False
                         return False
                     elif new_v1 is True:
+                        # first is True, return second
                         return new_v2
                     elif new_v2 is True:
+                        # second is True, return first
                         return new_v1
 
-                elif self.type == "->":
+                elif self.type == "->":  # the statement was an implication
                     if new_v1 is True:
+                        # first is true, return second
                         return new_v2
                     elif new_v1 is False:
+                        # first is False, return True
                         return True
                     elif new_v2 is True:
+                        # second is True, return True
                         return True
                     elif new_v2 is False:
+                        # second is False, return negation of first
                         return Statement("~", new_v1)
 
-
-                elif self.type == "<->":
+                elif self.type == "<->":  # the statement was a biconditional
                     if new_v1 is True:
+                        # first is True, return second
                         return new_v2
                     elif new_v1 is False:
+                        # first is False, return negation of second
                         return Statement("~", new_v2)
                     elif new_v2 is True:
+                        # second is True, return first
                         return new_v1
                     elif new_v2 is False:
+                        # second is False, return negation of first
                         return Statement("~", new_v1)
 
+                # there were no reductions to a boolean value above, so just return the statement with the reduced parts and the same connector
                 return Statement(self.type, new_v1, new_v2)
