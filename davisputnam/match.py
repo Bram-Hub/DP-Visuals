8df196d7cc766c3034397e9a43dfcbe896722285
Shane Aston <astonshane@gmail.com>
2016-03-18 14:04:30 -0400

    comments for match/parse

--- davisputnam/match.py
+++ davisputnam/match.py
@@ -1,45 +1,45 @@
 import re
 
 
-# takes a string and matches it to its "parts" and labels the main connective
+# takes a string and matches it to its "parts" and labels the main connective using regex
 def match(test_str):
-    # check against a plain literal
+    # check against a pure literal (A)
     literalRE = re.compile(ur'^([A-Z])$')
     match = re.search(literalRE, test_str)
     if match:
         return ["lit", match.group(1)]
 
-    # check against a basic negation
+    # check against a basic negation (~A)
     negationRE = re.compile(ur'^~([A-Z])$')
     match = re.search(negationRE, test_str)
     if match:
         return ["~", match.group(1)]
 
-    # check against a more complex negation
+    # check against a more complex negation (~(AvB))
     negationRE = re.compile(ur'^~\(([A-Zv\^~\<\-\>\(\)]+)\)$')
     match = re.search(negationRE, test_str)
     if match:
         return ["~", match.group(1)]
 
-    # check agains a binary connector
+    # check against a binary connector (AvB)^(AvB)
     binaryRE = re.compile(ur'^\(([A-Zv\^\-\>\<~\(\)]+)\)(v|\^|\-\>|\<\-\>)\(([A-Zv\^\-\>\<~\(\)]+)\)$')
     match = re.search(binaryRE, test_str)
     if match:
         return [match.group(2), match.group(1, 3)]
 
-    # check agains a binary connector
+    # check against a binary connector (AvB)^A, (AvB)^~A, (AvB)^~(AvB)
     binaryRE = re.compile(ur'^\(([A-Zv\^\-\>\<~\(\)]+)\)(v|\^|\-\>|\<\-\>)([A-Z]|~[A-Z]|~\([A-Zv\^\-\>\<~\(\)]+\))$')
     match = re.search(binaryRE, test_str)
     if match:
         return [match.group(2), match.group(1, 3)]
 
-    # check agains a binary connector
+    # check against a binary connector ...
     binaryRE = re.compile(ur'^([A-Z]|~[A-Z]|~\([A-Zv\^\-\>\<~\(\)]+\))(v|\^|\-\>|\<\-\>)\(([A-Zv\^\-\>\<~\(\)]+)\)$')
     match = re.search(binaryRE, test_str)
     if match:
         return [match.group(2), match.group(1, 3)]
 
-    # check agains a binary connector
+    # check against a binary connector ...
     binaryRE = re.compile(ur'^([A-Z]|~[A-Z]|~\([A-Zv\^\-\>\<~\(\)]+\))(v|\^|\-\>|\<\-\>)([A-Z]|~[A-Z]|~\([A-Zv\^\-\>\<~\(\)]+\))$')
     match = re.search(binaryRE, test_str)
     if match:
@@ -47,6 +47,6 @@ def match(test_str):
 
     return None
 
-
+# regex:                                     # matches:
 #  \(([A-Zv\^\-\>\<~]+)\)                    # (AvB)
 #  ([A-Z]|~[A-Z]|~\([A-Zv\^\-\>\<~]+\))      # A     ~A      ~(AvB)
--- davisputnam/parse.py
+++ davisputnam/parse.py
@@ -2,12 +2,16 @@ import match
 import statement
 
 
+# recursively parse a statement from a string
 def parse(test_str):
+    # match the string to its main connector
     val = match.match(test_str)
     if val is None:
         return None
     else:
+        # split the result into a connector type and the internal matches
         [kind, matches] = val
+        # construct a statement (recursively) based on the connector type
         if kind == "lit":
             return statement.Statement(kind, matches)
         elif kind == "~":
